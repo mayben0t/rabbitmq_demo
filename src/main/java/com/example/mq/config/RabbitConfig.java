b62939f0bd751c7402974f877f56412250425296
4某人 <594180536@qq.com>
2019-05-23 00:10:03 +0800

    第一个消息队列

--- src/main/java/com/example/mq/config/RabbitConfig.java
+++ src/main/java/com/example/mq/config/RabbitConfig.java
@@ -3,6 +3,10 @@ package com.example.mq.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -72,4 +76,37 @@ public class RabbitConfig {
         return template;
     }
 
+    /**
+     * 针对消费者配置
+     * 1. 设置交换机类型
+     * 2. 将队列绑定到交换机
+     FanoutExchange: 将消息分发到所有的绑定队列，无routingkey的概念
+     HeadersExchange ：通过添加属性key-value匹配
+     DirectExchange:按照routingkey分发到指定队列
+     TopicExchange:多关键字匹配
+     */
+    @Bean
+    public DirectExchange defaultExchange() {
+        return new DirectExchange(EXCHANGE_A);
+    }
+
+    /**
+     * 获取队列A
+     * @return
+     */
+    @Bean
+    public Queue queueA() {
+        return new Queue(QUEUE_A, true); //队列持久
+    }
+
+    @Bean
+    public Binding binding() {
+
+        return BindingBuilder.bind(queueA()).to(defaultExchange()).with(RabbitConfig.ROUTINGKEY_A);
+    }
+
+//    @Bean
+//    public Binding bindingB(){
+//        return BindingBuilder.bind(queueB()).to(defaultExchange()).with(RabbitConfig.ROUTINGKEY_B);
+//    }
 }
--- src/main/java/com/example/mq/controller/TestController.java
+++ src/main/java/com/example/mq/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.example.mq.controller;
 
 
+import com.example.mq.service.MsgProducer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/test")
 public class TestController {
+    @Autowired
+    MsgProducer msgProducer;
 
     @GetMapping("/hi")
     public String hello(){
+        msgProducer.sendMsg("cross the great wall,we can arrive anywhere on the world");
         return "hi";
     }
+
+
 }
